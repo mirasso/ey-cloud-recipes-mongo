0d267baf459043f06473d66ada40db84d8eacf16
Hiroyuki Yamaoka <yamaoka@428lab.jp>
2013-06-09 23:23:37 +0900

    add nginx customization

---
+++ cookbooks/nginx-custom/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: nginx-custom
+# Recipe:: default
+#
+
+if ['app_master', 'app'].include? node[:instance_role]
+  node[:engineyard][:environment][:apps].each do |app|
+    cookbook_file 'custom.conf' do
+      path "/etc/nginx/servers/#{app[:name]}/custom.conf"
+      source 'custom.conf'
+      owner node[:users][0][:username]
+      group node[:users][0][:username]
+      mode 0644
+    end
+    cookbook_file 'comap.users' do
+      path '/etc/nginx/servers/comap.users'
+      source 'comap.users'
+      owner node[:users][0][:username]
+      group node[:users][0][:username]
+      mode 0644
+    execute "sudo /etc/init.d/nginx reload"
+  end
+end
