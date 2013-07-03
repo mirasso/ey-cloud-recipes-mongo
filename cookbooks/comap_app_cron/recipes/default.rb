a87dc571ae7228786efde8cc4e89c337555f1791
Hiroyuki Yamaoka <yamaoka@428lab.jp>
2013-07-04 00:59:45 +0900

    add create_daily_report task

--- cookbooks/comap_app_cron/recipes/default.rb
+++ cookbooks/comap_app_cron/recipes/default.rb
@@ -10,3 +10,12 @@ if instance_role?(:app_master, :app, :solo)
     command 'nice find /data/comap/shared/tmp/ -amin +10 -type f | xargs rm -f > /dev/null 2>&1'
   end
 end
+
+if instance_role?(:app_master)
+  cron "create_daily_report" do
+    minute '30'
+    hour '1'
+    user 'deploy'
+    command 'nice php /data/comap/current/cli/create_daily_report.php > /data/comap/shared/log/daily_report.log 2>&1'
+  end
+end
